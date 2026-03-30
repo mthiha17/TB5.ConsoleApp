fc038c8fa9835872e25e7655bf6bfa76cbd382db
Min Thiha <appleopen1@gmail.com>
2026-03-30 17:12:42 +0630

    EFCore

---
+++ TB5.ConsoleApp.EFCoreSample/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TB5.ConsoleApp.EFCoreSample;
+
+Console.WriteLine("Hello, World!");
+
+EFCoreService service = new EFCoreService();
+service.Create();
+//service.Read();
+//service.Update();
+//service.Edit();
+//service.Delete();
+
+
+Console.ReadLine();
